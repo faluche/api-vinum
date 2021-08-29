7f523eb59b92685e65e097a15d6a194793729f78
Alexandre Moreau <moreau.alexandre23@gmail.com>
2021-08-29 09:51:46 +0200

    init project

---
+++ src/main/java/io/faluche/vinum/VinumApplication.java
@@ -0,0 +1,13 @@
+package io.faluche.vinum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/faluche/vinum/VinumApplicationTests.java
@@ -0,0 +1,13 @@
+package io.faluche.vinum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
